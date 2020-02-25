af7ab190681b159a1966339f46c40233a20c8fa4
ALamgir Hossain <alamgir.cse14.just@gmail.com>
2020-02-25 19:10:49 +0600

    Update coming-soon.js

--- Student Faculty File Sharing/WebContent/js/coming-soon.js
+++ Student Faculty File Sharing/WebContent/js/coming-soon.js
@@ -1,12 +1,12 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  // Vide - Video Background Settings
+  // Vide - Video Background Settings, by Alamgir
   $('body').vide({
     mp4: "mp4/bg.mp4",
     poster: "img/bg-mobile-fallback.jpg"
   }, {
     posterType: 'jpg'
   });
-
+//Alamgir, CSE, JUST
 })(jQuery); // End of use strict
